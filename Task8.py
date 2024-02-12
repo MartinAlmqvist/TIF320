df6415f2cbf4526ec2cb909674121901a64a757c
Lovisaakesson <98884751+Lovisaakesson@users.noreply.github.com>
2024-02-12 12:46:00 +0100

    Update Task8.py

--- Task8.py
+++ Task8.py
@@ -6,7 +6,6 @@ from ase.optimize import GPMin
 from ase.io import read, write
 import numpy as np
 
-
 try:
     db6 = connect("A2_6/gadb.db")
     
@@ -17,17 +16,20 @@ try:
     if entry is not None:
         atoms6_lowest = entry.toatoms() #id 193
         atom6_second_lowest  = entry2.toatoms() # id 24
-        write("atoms6_minimum.xyz", atoms6_lowest)
-        write("atom6_24.xyz", atom6_second_lowest)
-        print("Atoms written to atoms6_minimum.xyz")
     else:
         print("No matching entry found in the database for ID 193 for A6.")
     
 except Exception as e:
     print("An error occurred:", e)
 
+# save the lattice to an xyz file
+#write("Task8_6atoms_lowest_Before.xyz",atoms6_lowest)
+#write("Task8_6atoms_second_lowest_Before.xyz",atoms6_lowest)
 
-write("Task8_6atoms_Before.xyz",atoms6_lowest)
+#calculate initial energy before relaxation
+initial_energy_lowest = atoms6_lowest.get_potential_energy()
+initial_energy_second_lowest = atom6_second_lowest.get_potential_energy()
+
 # Code from ga.py
 calc = GPAW(nbands=10, #Number of electronic bands
             h=0.25, #Grid spacing [Å]
@@ -39,21 +41,28 @@ calc = GPAW(nbands=10, #Number of electronic bands
 
 # Set up the structure in GPAW
 atoms6_lowest.set_calculator(calc)
-
-# Save the electron density to a cube file
-#calc.write('electron_density.cube', data=calc.get_all_electron_density())
+atom6_second_lowest.set_calculator(calc)
 
 # Relax
-dyn = GPMin(atoms6_lowest, trajectory='relax_ref.traj', logfile='relax_ref.log')
-dyn.run(fmax=0.02, steps=100)
+dyn_lowest = GPMin(atoms6_lowest, trajectory='relax_ref_lowest.traj', logfile='relax_ref_lowest.log')
+dyn_lowest.run(fmax=0.02, steps=100)
+
+dyn_second_lowest = GPMin(atom6_second_lowest, trajectory='relax_ref_second_lowest.traj', logfile='relax_ref_second_lowest.log')
+dyn_second_lowest.run(fmax=0.02, steps=100)
 
 # Get the total energy of the relaxed structure
-total_energy = atoms6_lowest.get_potential_energy()
-write("Task8_6atoms_After.xyz",atoms6_lowest)
+final_energy_lowest = atoms6_lowest.get_potential_energy()
+final_energy_second_lowest = atom6_second_lowest.get_potential_energy()
+
+# save the relaxed lattice to an xyz file
+#write("Task8_6atoms_lowest_After.xyz",atoms6_lowest)
+#write("Task8_6atoms_second_lowest_After.xyz",atom6_second_lowest)
 
 # Save the wavefunction in a .gpw file
 #calc.write('na_atoms_wavefunction.gpw')
 
-f = open("total_energy.txt", "w")
-f.write(f'Total energy: {total_energy} eV')
-f.close()    
+with open("energy_before_after.txt", "w") as f:
+    f.write(f'Initial energy lowest: {initial_energy_lowest} eV\n')
+    f.write(f'Final energy lowest: {final_energy_lowest} eV\n')
+    f.write(f'Initial energy second lowest: {final_energy_lowest} eV\n')
+    f.write(f'Final energy second lowest: {final_energy_second_lowest} eV\n')
