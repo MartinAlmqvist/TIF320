cdd41df6beced7471276948279201654829f2023
Lovisa <lollo.99@hotmail.com>
2024-02-14 14:52:30 +0100

    Added A3

---
+++ A3/A3_Task1.py
@@ -0,0 +1,41 @@
+from ase.db import connect
+from ase.io import read, write
+from ase import Atoms
+from gpaw import GPAW, FermiDirac, NPT
+from ase.optimize import GPMin
+import numpy as np
+from ase.units import fs, kB
+from ase.io.trajectory import Trajectory
+
+
+atoms = read("Task1.xyz")
+calc = GPAW(
+    nbands=10, #Number of electronic bands
+    h=0.25, #Grid spacing [Å]
+    mode = 'lcao',
+    xc = 'PBE',
+    basis = 'dzp',
+    symmetry = {'point_group': False }, # Turn off point - group symmetry
+    charge = 251, # Charged system
+    txt = 'output.gpaw-out' # Redirects calculator output to this file !
+    )
+
+atoms.set_calculator(calc)
+
+dyn = NPT( # Some MD method
+    atoms,
+    temperature_K = 350 ,
+    timestep = 200* fs , # 200 fs equals 2ps of runtime is 10 steps of the MD simulation is made
+    ttime = 20*fs , # Don ’t forget the fs !
+    externalstress = 0 , # We don ’t use the barostat , but this needs to be set anyway !
+    logfile = 'mdOutput.log',  # Outputs temperature ( and more ) to file at each timestep
+    pfactor = None 
+    )
+
+trajectory = Trajectory('A3_Task1.traj', 'w' , atoms)
+dyn.attach(trajectory.write , interval =1) # Write the current positions etc . to file each timestep
+
+dyn.run(10) # Run 10 steps of MD simulation
+
+write('A3_Task1_After.xyz', atoms)
+calc.write('atoms_wavefunction.gpw')
---
+++ A3/Task_1_preparation.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ase.io.trajectory import Trajectory
+from ase.io import read, write
+
+def set_plot_style():
+    sns.set_context("paper", font_scale=2)
+    sns.set_style("darkgrid")
+    sns.set_palette("deep")
+    sns.set(font='sans-serif')
+
+    fig_width = 8
+    fig_height = 6
+
+    plt.rcParams['figure.figsize'] = (fig_width, fig_height)
+    plt.rcParams['figure.dpi'] = 100
+    plt.rcParams['lines.linewidth'] = 1
+    plt.rcParams['lines.markersize'] = 4
+    plt.rcParams['font.size'] = 2.5 * fig_width
+    plt.rcParams['xtick.labelsize'] = 1.8 * fig_height
+    plt.rcParams['ytick.labelsize'] = 1.8 * fig_height
+    plt.grid(True)
+
+set_plot_style()
+
+data = np.genfromtxt('cluster24.txt', dtype=float)
+
+
+# Plotting
+x = data[:, 0]
+temp = data[:, 4]
+E_tot = data[:, 1]
+E_pot =data[:, 2]
+E_kin = data[:, 3]
+
+print(len(temp))
+length = 50
+bath_size = int(len(temp)/length)
+print(bath_size)
+
+for i in range(length):
+    
+    average = np.mean(temp[i*bath_size:i*bath_size + bath_size])
+    print(average)
+    
+
+traj = Trajectory('cluster24.traj')
+index = int(len(traj)*(4/6))
+atoms = traj[index]
+
+write("Task1.xyz",atoms)
+
+
+plt.plot(x, temp)
+plt.xlabel('Time [ps]')
+plt.ylabel('Temperature [K]')
+plt.title('Temperature Convergence over Time')
+plt.savefig('Figures/Task_1_temp.png')
+plt.show()
+
+plt.plot(x, E_tot, label = 'Total Energy')
+plt.plot(x, E_pot, label = 'Potential Energy')
+plt.plot(x, E_kin, label = 'Kinetic Energy')
+plt.xlabel('Time [ps]')
+plt.ylabel('Energy [eV]')
+plt.title('Energy Convergence over Time')
+plt.legend()
+plt.savefig('Figures/Task_1_energy.png')
+plt.show() 
