90b0c9c4d36a8b9a3a348c85111ccc57b8c04f03
MartinAlmqvist <martin.almqvist93@gmail.com>
2024-03-06 11:07:16 +0100

    finished task5

--- A4/Task5.py
+++ A4/Task5.py
@@ -1,105 +1,49 @@
-# import numpy as np
-# from ase.build import molecule
-# from ase.vibrations import Vibrations
-# from gpaw import GPAW, PW
-# from ase import Atoms
-# from ase.optimize import BFGS
-# from ase.io import write
-
-
-# # Define function to get GPAW calculator
-# def get_calc(element):
-#     calc = GPAW(xc='PBE',  
-#                 mode=PW(450), 
-#                 kpts=(1, 1, 1),  # gamma-point
-#                 spinpol=True,  # non-spin-polarized calculation
-#                 txt=f'{element}_Task5.txt')
-#     return calc
-
-# # Define the size of the box
-# box_size = 12.0  # Å
-
-# # Create O2 and CO molecules
-# O2 = molecule("O2", cell=(box_size, box_size, box_size))
-# CO = molecule("CO", cell=(box_size, box_size, box_size))
-# O2.center()
-# CO.center()
-
-# # Get GPAW calculators
-# calc_O2 = get_calc("O2")
-# calc_CO = get_calc("CO")
-
-# # Set calculators for the molecules
-# O2.set_calculator(calc_O2)
-# CO.set_calculator(calc_CO)
-
-# # Optimize the structures
-# BFGS(O2).run(fmax=0.01)
-# BFGS(CO).run(fmax=0.01)
-
-# # Perform vibrational analysis
-# vib_O2 = Vibrations(O2)
-# vib_CO = Vibrations(CO)
-# vib_O2.run()
-# vib_CO.run()
-
-# # Write summary files
-# vib_O2.summary(log='O2_vibrations_summary.txt')
-# vib_CO.summary(log='CO_vibrations_summary.txt')
-
-# # Write vibrational modes to files
-# vib_O2.write_mode(-1)
-# vib_CO.write_mode(-1)
-
-# # Save the vibrational mode files
-# write("O2_modes.xyz", vib_O2)
-# write("CO_modes.xyz", vib_CO)
-import numpy as np
 from ase.build import molecule
-from gpaw import GPAW, PW
+from ase.calculators.emt import EMT
+from ase.optimize import BFGS
 from ase.vibrations import Vibrations
+from ase.thermochemistry import IdealGasThermo
+from ase.io import read
 
-# Define function to get GPAW calculator
-def get_calc(element):
-    calc = GPAW(xc='PBE',  
-                mode=PW(450), 
-                kpts=(1, 1, 1),  # gamma-point
-                spinpol=False,  # non-spin-polarized calculation
-                txt=f'{element}_Task5.txt')
-    return calc
+atoms = read('E_O2_pot_relaxed_structure.xyz')
+atoms.calc = EMT()
+BFGS(atoms).run(fmax=0.01)
 
-# Create gas-phase molecule
-molecule_name = "O2"  # Change to the molecule you want to analyze
-gas_molecule = molecule(molecule_name)
+vib = Vibrations(atoms)
+vib.run()
+vib.summary(log='E_O2_EMT_summary.txt')
+vib.write_mode(-1)
+vib_energies = vib.get_energies()
+potentialenergy = atoms.get_potential_energy()
 
-# Get GPAW calculator
-calc_gas = get_calc(molecule_name)
+thermo = IdealGasThermo(vib_energies=vib_energies,
+                        potentialenergy=potentialenergy,
+                        atoms=atoms,
+                        geometry='linear',
+                        symmetrynumber=2, spin=0)
 
-# Set calculator for the gas-phase molecule
-gas_molecule.set_calculator(calc_gas)
+Entropy = thermo.get_entropy(temperature=298.15, pressure=101325.)
+with open(f"Energy_CO_task5.txt", "a") as elementfile:
+    elementfile.write(str(Entropy) + '\n')
 
-# Optimize the gas-phase molecule
-gas_molecule.calc.set(fmax=0.01)
-dyn = gas_molecule.calc.get_dynamics(fmax=0.01)
-dyn.run(0.1)  # Run the dynamics for a short time to relax the structure
+atoms = read('E_O2_pot_relaxed_structure.xyz')
+atoms.calc = EMT()
+BFGS(atoms).run(fmax=0.01)
 
-# Perform vibrational analysis
-vib_gas = Vibrations(gas_molecule)
-vib_gas.run()
+vib = Vibrations(atoms)
+vib.run()
+vib.summary(log='E_O2_EMT_summary.txt')
+vib.write_mode(-1)
+vib_energies = vib.get_energies()
+potentialenergy = atoms.get_potential_energy()
 
-# Access additional functionalities
-# Fold frequencies and intensities within a given range
-folded_freq, folded_intensity = vib_gas.fold(start=800.0, end=4000.0, npts=None, width=4.0, type='Gaussian', normalize=False)
+thermo = IdealGasThermo(vib_energies=vib_energies,
+                        potentialenergy=potentialenergy,
+                        atoms=atoms,
+                        geometry='linear',
+                        symmetrynumber=2, spin=0)
 
-# Get vibration energies in eV
-energies = vib_gas.get_energies()
-
-# Get vibration frequencies in cm^-1
-frequencies = vib_gas.get_frequencies()
-
-
-# Get specific vibration mode
-mode_number = 0  # Change to the mode number you want to retrieve
-mode = vib_gas.get_mode(mode_number)
-
+Entropy = thermo.get_entropy(temperature=300, pressure=101325.)
+with open(f"Energy_O2_task5.txt", "a") as elementfile:
+    elementfile.write(str(Entropy) + '\n')
 
